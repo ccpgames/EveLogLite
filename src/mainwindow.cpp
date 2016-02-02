2f77b39662b9c6468941e72c4a685df618d9681f
Gabríel Arthúr Pétursson <gabrielp@ccpgames.com>
2016-02-02 20:09:46 +0000

    Add license and link to source code in the about dialog

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -660,7 +660,19 @@ void MainWindow::showSettings()
 
 void MainWindow::showAboutDialog()
 {
-    QMessageBox::about(this, "EVE LogLite", "EVE LogLite server version " APP_VERSION "\nCopyright CCP 2015");
+    QMessageBox::about(this, "EVE LogLite",
+        "EVE LogLite server version " APP_VERSION "\n"
+        "\n"
+        "Copyright © 2015, 2016 CCP hf.\n"
+        "\n"
+        "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n"
+        "\n"
+        "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n"
+        "\n"
+        "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n"
+        "\n"
+        "Source code is available at https://github.com/ccpgames/EveLogLite"
+    );
 }
 
 void MainWindow::updateTaskbarIcon()
