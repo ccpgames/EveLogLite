0efb298266ebb76d2ea5c61e81212579c2c0bdcc
filipppavlov <filipp.pavlov@gmail.com>
2016-11-17 09:54:15 +0000

    fixed https://github.com/ccpgames/EveLogLite/issues/6

--- src/logmonitorfilemodel.cpp
+++ src/logmonitorfilemodel.cpp
@@ -119,148 +119,155 @@ void LogMonitorFileModel::clear()
 
 bool LogMonitorFileModel::saveModel(AbstractLogModel *model, const QString& fileName)
 {
-    auto db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName(fileName);
-    auto success = db.open();
-    if (!success)
+    QTemporaryFile tempFile;
+    tempFile.open();
+
     {
-        return false;
-    }
-
-    auto check = [&](bool result)
-    {
-        if (!result)
+        auto db = QSqlDatabase::addDatabase("QSQLITE");
+        db.setDatabaseName(tempFile.fileName());
+        auto success = db.open();
+        if (!success)
         {
-            auto e = db.lastError();
-            qDebug() << "Query failed: " << e.text() << e.nativeErrorCode();
+            return false;
         }
-    };
 
-    check(QSqlQuery(db).exec("CREATE TABLE lsw(version INT)"));
-    check(QSqlQuery(db).exec("CREATE TABLE messages(time REAL, host INT, pid INT, level INT, channel INT, message TEXT)"));
-    check(QSqlQuery(db).exec("CREATE TABLE hosts(id INT,name TEXT)"));
-    check(QSqlQuery(db).exec("CREATE TABLE processes(id INT, module TEXT, process TEXT, host INT)"));
-    check(QSqlQuery(db).exec("CREATE TABLE channels(id INT,facility TEXT,object TEXT)"));
-    check(QSqlQuery(db).exec("CREATE VIEW log as "
-              "select m.rowid, '' as timestamp, m.time, h.name as host, m.pid, m.level, m.level as type, p.module, c.facility || '-' || c.object as channel, m.message, p.process "
-                  "from messages as m, hosts as h, processes as p, channels as c "
-                  "where h.id = m.host and p.id = m.pid and c.id = m.channel"));
-
-
-
-    check(QSqlQuery(db).exec("INSERT INTO lsw VALUES (3)"));
-
-    int count = model->rowCount();
-    QMap<QString, int> hosts;
-    QMap<quint64, QString> processes;
-    QMap<QPair<QString, QString>, int> channels;
-
-    for (int i = 0; i < count; ++i)
-    {
-        auto message = model->message(i);
-        if (!message || message->isMultilineContinuation)
+        auto check = [&](bool result)
         {
-            continue;
-        }
-        if (hosts.find(message->machineName) == hosts.end())
-        {
-            hosts[message->machineName] = hosts.size() + 1;
-        }
-        processes[message->pid] = message->executablePath;
-        if (channels.find(QPair<QString, QString>(message->module, message->channel)) == channels.end())
-        {
-            channels[QPair<QString, QString>(message->module, message->channel)] = channels.size() + 1;
-        }
-    }
+            if (!result)
+            {
+                auto e = db.lastError();
+                qDebug() << "Query failed: " << e.text() << e.nativeErrorCode();
+            }
+        };
 
-    check(QSqlQuery(db).exec("BEGIN TRANSACTION"));
-    {
-        QSqlQuery q(db);
-        q.prepare("INSERT INTO hosts VALUES (?, ?)");
+        check(QSqlQuery(db).exec("CREATE TABLE lsw(version INT)"));
+        check(QSqlQuery(db).exec("CREATE TABLE messages(time REAL, host INT, pid INT, level INT, channel INT, message TEXT)"));
+        check(QSqlQuery(db).exec("CREATE TABLE hosts(id INT,name TEXT)"));
+        check(QSqlQuery(db).exec("CREATE TABLE processes(id INT, module TEXT, process TEXT, host INT)"));
+        check(QSqlQuery(db).exec("CREATE TABLE channels(id INT,facility TEXT,object TEXT)"));
+        check(QSqlQuery(db).exec("CREATE VIEW log as "
+                  "select m.rowid, '' as timestamp, m.time, h.name as host, m.pid, m.level, m.level as type, p.module, c.facility || '-' || c.object as channel, m.message, p.process "
+                      "from messages as m, hosts as h, processes as p, channels as c "
+                      "where h.id = m.host and p.id = m.pid and c.id = m.channel"));
 
-        QVariantList ids;
-        QVariantList names;
-        for (auto it = hosts.begin(); it != hosts.end(); ++it)
-        {
-            ids << it.value();
-            names << it.key();
-        }
-        q.addBindValue(ids);
-        q.addBindValue(names);
-        check(q.execBatch());
-    }
-    {
-        QSqlQuery q(db);
-        q.prepare("INSERT INTO processes VALUES (?, ?, ?, ?)");
 
-        QVariantList ids;
-        QVariantList modules;
-        QVariantList process;
-        QVariantList hosts;
-        for (auto it = processes.begin(); it != processes.end(); ++it)
-        {
-            ids << it.key();
-            modules << "";
-            process << it.value();
-            hosts << 0;
-        }
-        q.addBindValue(ids);
-        q.addBindValue(modules);
-        q.addBindValue(process);
-        q.addBindValue(hosts);
-        check(q.execBatch());
-    }
-    {
-        QSqlQuery q(db);
-        q.prepare("INSERT INTO channels VALUES (?, ?, ?)");
 
-        QVariantList ids;
-        QVariantList facilities;
-        QVariantList objects;
-        for (auto it = channels.begin(); it != channels.end(); ++it)
-        {
-            ids << it.value();
-            facilities << it.key().first;
-            objects << it.key().second;
-        }
-        q.addBindValue(ids);
-        q.addBindValue(facilities);
-        q.addBindValue(objects);
-        check(q.execBatch());
-    }
-    {
-        QSqlQuery q(db);
-        q.prepare("INSERT INTO messages VALUES (?, ?, ?, ?, ?, ?)");
+        check(QSqlQuery(db).exec("INSERT INTO lsw VALUES (3)"));
 
-        QVariantList time;
-        QVariantList host;
-        QVariantList pid;
-        QVariantList level;
-        QVariantList channel;
-        QVariantList message;
+        int count = model->rowCount();
+        QMap<QString, int> hosts;
+        QMap<quint64, QString> processes;
+        QMap<QPair<QString, QString>, int> channels;
+
         for (int i = 0; i < count; ++i)
         {
-            auto msg = model->message(i);
-            if (!msg || msg->isMultilineContinuation)
+            auto message = model->message(i);
+            if (!message || message->isMultilineContinuation)
             {
                 continue;
             }
-            time << double(msg->timestamp.toMSecsSinceEpoch()) / 1000;
-            host << hosts[msg->machineName];
-            pid << msg->pid;
-            level << msg->severity;
-            channel << channels[QPair<QString, QString>(msg->module, msg->channel)];
-            message << msg->originalMessage;
+            if (hosts.find(message->machineName) == hosts.end())
+            {
+                hosts[message->machineName] = hosts.size() + 1;
+            }
+            processes[message->pid] = message->executablePath;
+            if (channels.find(QPair<QString, QString>(message->module, message->channel)) == channels.end())
+            {
+                channels[QPair<QString, QString>(message->module, message->channel)] = channels.size() + 1;
+            }
         }
-        q.addBindValue(time);
-        q.addBindValue(host);
-        q.addBindValue(pid);
-        q.addBindValue(level);
-        q.addBindValue(channel);
-        q.addBindValue(message);
-        check(q.execBatch());
+
+        check(QSqlQuery(db).exec("BEGIN TRANSACTION"));
+        {
+            QSqlQuery q(db);
+            q.prepare("INSERT INTO hosts VALUES (?, ?)");
+
+            QVariantList ids;
+            QVariantList names;
+            for (auto it = hosts.begin(); it != hosts.end(); ++it)
+            {
+                ids << it.value();
+                names << it.key();
+            }
+            q.addBindValue(ids);
+            q.addBindValue(names);
+            check(q.execBatch());
+        }
+        {
+            QSqlQuery q(db);
+            q.prepare("INSERT INTO processes VALUES (?, ?, ?, ?)");
+
+            QVariantList ids;
+            QVariantList modules;
+            QVariantList process;
+            QVariantList hosts;
+            for (auto it = processes.begin(); it != processes.end(); ++it)
+            {
+                ids << it.key();
+                modules << "";
+                process << it.value();
+                hosts << 0;
+            }
+            q.addBindValue(ids);
+            q.addBindValue(modules);
+            q.addBindValue(process);
+            q.addBindValue(hosts);
+            check(q.execBatch());
+        }
+        {
+            QSqlQuery q(db);
+            q.prepare("INSERT INTO channels VALUES (?, ?, ?)");
+
+            QVariantList ids;
+            QVariantList facilities;
+            QVariantList objects;
+            for (auto it = channels.begin(); it != channels.end(); ++it)
+            {
+                ids << it.value();
+                facilities << it.key().first;
+                objects << it.key().second;
+            }
+            q.addBindValue(ids);
+            q.addBindValue(facilities);
+            q.addBindValue(objects);
+            check(q.execBatch());
+        }
+        {
+            QSqlQuery q(db);
+            q.prepare("INSERT INTO messages VALUES (?, ?, ?, ?, ?, ?)");
+
+            QVariantList time;
+            QVariantList host;
+            QVariantList pid;
+            QVariantList level;
+            QVariantList channel;
+            QVariantList message;
+            for (int i = 0; i < count; ++i)
+            {
+                auto msg = model->message(i);
+                if (!msg || msg->isMultilineContinuation)
+                {
+                    continue;
+                }
+                time << double(msg->timestamp.toMSecsSinceEpoch()) / 1000;
+                host << hosts[msg->machineName];
+                pid << msg->pid;
+                level << msg->severity;
+                channel << channels[QPair<QString, QString>(msg->module, msg->channel)];
+                message << msg->originalMessage;
+            }
+            q.addBindValue(time);
+            q.addBindValue(host);
+            q.addBindValue(pid);
+            q.addBindValue(level);
+            q.addBindValue(channel);
+            q.addBindValue(message);
+            check(q.execBatch());
+        }
+        check(QSqlQuery(db).exec("END TRANSACTION"));
+        db.close();
     }
-    check(QSqlQuery(db).exec("END TRANSACTION"));
-    db.close();
-    return true;
+    QSqlDatabase::removeDatabase("QSQLITE");
+
+    return tempFile.copy(fileName);
 }
