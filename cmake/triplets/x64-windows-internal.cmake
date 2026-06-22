1951ccb83f9275e106bd463f04598806f6a91b6a
Hrafn <5185245+hrafn@users.noreply.github.com>
2026-06-22 10:47:46 +0000

    Use 145 toolset when building on Windows
    The version of Qt we want to use requires at least Visual Studio 2022 (MSVC 1930 or newer).

--- cmake/triplets/x64-windows-internal.cmake
+++
@@ -1,8 +0,0 @@
-# Copyright © 2026 Fenris Creations.
-set(BASE_TRIPLET_DIR "${CMAKE_CURRENT_LIST_DIR}/../../vendor/github.com/carbonengine/vcpkg-registry/triplets")
-set(BASE_TRIPLET_FILE "${BASE_TRIPLET_DIR}/x64-windows-internal.cmake")
-list(APPEND VCPKG_HASH_ADDITIONAL_FILES ${BASE_TRIPLET_FILE})
-include(${BASE_TRIPLET_FILE})
-
-set(VCPKG_LIBRARY_LINKAGE static)
-set(VCPKG_CRT_LINKAGE static)
