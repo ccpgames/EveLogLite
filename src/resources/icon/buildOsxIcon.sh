c1c650ce128fc8f696c2c04a95a7eafb1e345ce2
hrafng <5185245+hrafn@users.noreply.github.com>
2026-06-25 14:04:14 +0000

    Use custom command to generate icon set
    This means we can generate the icon set file once when we first configure, then skip the generation step when re-running configure.

--- src/resources/icon/buildOsxIcon.sh
+++ src/resources/icon/buildOsxIcon.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
-mkdir icon.iconset
-ls icon.iconset
+BINARY_DIR=$1
+ICONSET_DIR=$BINARY_DIR/icon.iconset
+mkdir -p $ICONSET_DIR
+SCRIPT_PATH=$(dirname $0)
 
-cp 16.png icon.iconset/icon_16x16.png
-cp 32.png icon.iconset/icon_16x16@2x.png
-cp 32.png icon.iconset/icon_32x32.png
-cp 64.png icon.iconset/icon_32x32@2x.png
-cp 64.png icon.iconset/icon_64x64.png
-cp 128.png icon.iconset/icon_64x64@2x.png
-cp 128.png icon.iconset/icon_128x128.png
-cp 256.png icon.iconset/icon_128x128@2x.png
-cp 256.png icon.iconset/icon_256x256.png
-cp 512.png icon.iconset/icon_256x256@2x.png
-cp 512.png icon.iconset/icon_512x512.png
-cp 1024.png icon.iconset/icon_512x512@2x.png
+cp $SCRIPT_PATH/16.png $ICONSET_DIR/icon_16x16.png
+cp $SCRIPT_PATH/32.png $ICONSET_DIR/icon_16x16@2x.png
+cp $SCRIPT_PATH/32.png $ICONSET_DIR/icon_32x32.png
+cp $SCRIPT_PATH/64.png $ICONSET_DIR/icon_32x32@2x.png
+cp $SCRIPT_PATH/64.png $ICONSET_DIR/icon_64x64.png
+cp $SCRIPT_PATH/128.png $ICONSET_DIR/icon_64x64@2x.png
+cp $SCRIPT_PATH/128.png $ICONSET_DIR/icon_128x128.png
+cp $SCRIPT_PATH/256.png $ICONSET_DIR/icon_128x128@2x.png
+cp $SCRIPT_PATH/256.png $ICONSET_DIR/icon_256x256.png
+cp $SCRIPT_PATH/512.png $ICONSET_DIR/icon_256x256@2x.png
+cp $SCRIPT_PATH/512.png $ICONSET_DIR/icon_512x512.png
+cp $SCRIPT_PATH/1024.png $ICONSET_DIR/icon_512x512@2x.png
 
-iconutil -c icns -o ../../icon.icns icon.iconset
-rm -R icon.iconset
-
+iconutil -c icns -o $BINARY_DIR/icon.icns $ICONSET_DIR
+rm -r $ICONSET_DIR
